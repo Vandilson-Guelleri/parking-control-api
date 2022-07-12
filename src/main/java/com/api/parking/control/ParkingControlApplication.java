240ae0996f87a71dfb4bd37a59173293a8c64440
Vandilson Guelleri <vandilsonguelleri@gmail.com>
2022-07-11 21:53:14 -0300

    Project created

---
+++ src/main/java/com/api/parking/control/ParkingControlApplication.java
@@ -0,0 +1,24 @@
+package com.api.parking.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+// @EntityScan("com.api.parking.models.ParkingSpotModel.java")
+@SpringBootApplication
+@RestController
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+	
+		}
+	
+		@GetMapping("/")
+		public String index() {
+			return "Olá mundo!!";
+		}
+	
+}
+
